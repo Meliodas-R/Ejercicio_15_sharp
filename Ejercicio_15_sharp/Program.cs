4aced1f9c8e2f3bc4b3c59e566c9275b87536881
Meliodas-R <inceptiongodmaxwell@gmail.com>
2021-11-02 11:20:39 +0100

    Agregada la función de conversión hexadecimal y la muestra del resultado por pantalla.

--- Ejercicio_15_sharp/Program.cs
+++ Ejercicio_15_sharp/Program.cs
@@ -6,7 +6,14 @@ namespace Ejercicio_15_sharp
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Introduzca un número: ");
+            string numero = Console.ReadLine();
+
+            int baseNumero = 10;
+            int baseConversion = 16;
+
+            string resultado = Convert.ToString(Convert.ToInt32(numero, baseNumero), baseConversion);
+            Console.WriteLine("\nEl número " + numero + " en hexadecimal es: " + resultado);
         }
     }
 }
